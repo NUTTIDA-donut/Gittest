b177be79901048b8b3ba8b59c95d9b59d77e2abb
Nuttida Meeboon <natthakan.t@mail.kmutt.ac.th>
2020-10-20 17:10:37 +0700

    new_featue

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -16,9 +16,15 @@ public class Gittest {
      */
     public static void main(String[] args) {
        sayHello();
+       sayGoodbye();
     }
     
     public static void sayHello(){
         System.out.println("Hello");
     }
+    
+    public static void sayGoodbye(){
+        System.out.println("Goodbye");
+    }
+    
 }
